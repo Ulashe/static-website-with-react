f42677ba78aeda9910395f09ab5d5df6f3aa750a
Ulas Ozturk <ozulashe@gmail.com>
2020-11-22 22:38:29 +0300

    It's working!!!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Helmet from "react-helmet";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Helmet title="Title setted with Helmet" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
